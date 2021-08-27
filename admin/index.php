41bdbbcbeaa012a4eec60d7c50eb9be395dca14b
Brayan Forero <brayanroswll2802@gmail.com>
2021-08-26 21:15:55 -0400

    add dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,7 @@
-<html lang="es">
-<head>
-	<title>Pedidos</title>
-</head>
-<body>
-	 <a href="../Registro_pedidos.php"> Pedido</a>
-      <a href="../Registro_producto.php"> Producto</a>
-      <a href="../Registro_cliente.php"> Cliente</a>  
-</body>
-	
-</html>
+<?php
+
+session_start();
+
+$user = $_SESSION['user'];
+
+include_once '../views/app.view.php';

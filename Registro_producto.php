5f77879751d48058faf8fc898fdcd1ab3d41f662
Brayan Forero <brayanroswll2802@gmail.com>
2021-08-28 22:02:23 -0400

    add dashboard products

--- Registro_producto.php
+++ Registro_producto.php
@@ -1,31 +1,37 @@
 <?php
 require_once "config/datos_conexion.php";
+session_start();
+$user = $_SESSION['user'] ?? ['username' => null];
 
-
-
-$conexion=mysqli_connect($db_host, $db_user, $db_pass);
+$conexion = mysqli_connect($db_host, $db_user, $db_pass);
 
 if (mysqli_connect_errno()) {
-	
-	echo "no se puede conectar a la base de datos";
-	
-	exit();
 
+	echo "no se puede conectar a la base de datos";
+
+	exit();
 }
-mysqli_select_db($conexion, $db_nombre)or die("no se encontro ninguna base de datos");
+mysqli_select_db($conexion, $db_nombre) or die("no se encontro ninguna base de datos");
 
 mysqli_set_charset($conexion, "utf8");
+
+$peticion = "SELECT * FROM productos";
+$resultado = mysqli_query($conexion, $peticion);
+
+
 ?>
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Registro_Producto</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<meta charset="utf-8">
+	<title>Registro_Producto</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="public/css/main.css">
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 </head>
 <style>
-	.btn_reg{
+	.btn_reg {
 		background: #0C6094;
 		color: white;
 		width: 100px;
@@ -35,7 +41,8 @@ mysqli_set_charset($conexion, "utf8");
 		border-radius: 5px;
 
 	}
-	.lbl_text{
+
+	.lbl_text {
 		width: 350px;
 		padding: 10px;
 		font-family: sans-serif;
@@ -44,55 +51,109 @@ mysqli_set_charset($conexion, "utf8");
 </style>
 
 <body>
-	<div align="center" style="margin-top: 50px;">
-	<div align="center" style="padding: 20px; color:black ; width:550px; border: 1px solid #aeadad;box-shadow: 0px 10px 15px #d9d0d0; ">
-		<div class="container">
-		<h4 align="center">Registro Productos</h4>
-		<form class="was-validated" action="php/insertar_producto.php" method="post" enctype="multipart/form-data" >
-			<div class="form-group">
-				<input class="form-control" type="text" name="nombre" placeholder="Ingrese un nombre" required>
-				<br>
-				<input class="form-control" type="text" name="cod" placeholder="Ingrese codigo" required>
-      			<br>
-				<input class="form-control" type="text" name="precio" placeholder="Ingrese precio" required>
-      			<br>
-				<input type="file" name="imagen"><br><br>
-				<a class="btn btn-primary" href="menu.php"><i class='fas fa-arrow-left'></i> Volver</a>
-				<input type="submit" name="reg_producto" value="Registrar" class="btn btn-primary">	
-			</div>	
-	</form>
+
+	<div class="container-fluid">
+		<div class="row justify-content-center align-content-center">
+			<div class="col-8 barra">
+				<h4 class="text-light">Logo</h4>
+			</div>
+			<div class="col-4 text-right barra">
+				<ul class="navbar-nav mr-auto">
+					<li>
+						<span href="#" class="px-3 text-light perfil dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fas fa-user-circle user mr-2"></i>
+							<?= $user['username'] ?>
+						</span>
+
+						<div class="dropdown-menu" aria-labelledby="navbar-dropdown">
+							<a class="dropdown-item menuperfil cerrar" href="php/logout.php"><i class="fas fa-sign-out-alt m-1"></i>Salir
+							</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="barra-lateral col-12 col-sm-auto">
+				<nav class="menu d-flex d-sm-block justify-content-center flex-wrap">
+					<a href="#"><i class="fas fa-home"></i><span>Inicio</span></a>
+					<a href="#"><i class="fas fa-home"></i><span>Productos</span></a>
+					<a href="#"><i class="fas fa-home"></i><span>Clientes</span></a>
+					<a href="#"><i class="fas fa-home"></i><span>Pedidos</span></a>
+				</nav>
+			</div>
+			<main class="main col col-md-10 pl-5">
+				<div class="row text-center">
+					<div class="columna col-12">
+						<!-- FORM -->
+						<div class="mb-3" align="center">
+							<div align="center" style="padding: 20px; color:black ; width:80%; border: 1px solid #aeadad;box-shadow: 0px 10px 15px #d9d0d0; ">
+								<div class="container">
+									<h4 align="center">Registro Productos</h4>
+									<form class="was-validated" action="php/insertar_producto.php" method="post" enctype="multipart/form-data">
+										<div class="form-group">
+											<input class="form-control" type="text" name="nombre" placeholder="Ingrese un nombre" required>
+											<br>
+											<input class="form-control" type="text" name="cod" placeholder="Ingrese codigo" required>
+											<br>
+											<input class="form-control" type="text" name="precio" placeholder="Ingrese precio" required>
+											<br>
+											<input type="file" name="imagen"><br><br>
+											<a class="btn btn-primary" href="menu.php"><i class='fas fa-arrow-left'></i> Volver</a>
+											<input type="submit" name="reg_producto" value="Registrar" class="btn btn-primary">
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<!-- FORM -->
+
+						<!-- TABLE -->
+						<div class="card shadow">
+							<table class="table table-sm .table-responsive">
+								<thead>
+									<tr>
+										<th scope="col">#</td>
+										<th scope="col">Producto</td>
+										<th scope="col">Codigo</th>
+										<th scope="col">Precio</th>
+										<th scope="col">Imagen</th>
+										<th scope="col">Actualizar</th>
+										<th scope="col">Borrar</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php while ($p = mysqli_fetch_array($resultado)) : ?>
+										<tr>
+											<form action="php/actualizar_producto.php?id=<?= $p['id'] ?>" method="post">
+												<th scope="row"><?= $p['id'] ?></th>
+												<td><input class="form-control" type="text" name="nombre" value="<?= $p['nombre'] ?>"></td>
+												<td><input class="form-control" type="text" name="cod" value="<?= $p['codigo'] ?>"></td>
+												<td><input class="form-control" type="text" name="precio" value="<?= $p['precio'] ?>"></td>
+												<td><img class="img-fluid rounded" width=" 80" src="imagen/<?= $p['imagen'] ?>" alt="image product"></td>
+												<td><button type="submit" class="btn btn-outline-success btn-sm">Actualizar</button></td>
+											</form>
+											<td><a href="php/eliminar_producto.php?id=<?= $p['id'] ?>" class="btn btn-outline-danger btn-sm">Borrar</a></td>
+										</tr>
+									<?php endwhile ?>
+								</tbody>
+							</table>
+						</div>
+						<!-- TABLE -->
+					</div>
+				</div>
+			</main>
+		</div>
 	</div>
-	</div>
-	<div class="container" style="margin-right: 2px;margin-left: 2px;">
-<table style="margin-top: 25px;" class="table table-bordered">
-	<tr>
-	<td><strong>Producto</strong></td>
-	<td><strong>Codigo</strong></td>
-	<td><strong>Precio</strong></td>
-	<td><strong>Imagen</strong></td>
-	<td><strong>Actualizar</strong></td>
-	<td><strong>Borrar</strong></td>
-</tr>
-<?php
-$peticion="SELECT * FROM productos";
-$resultado=mysqli_query($conexion,$peticion);
-while ($fila = mysqli_fetch_array($resultado)) {
-	echo '<tr>
-	<form action="php/actualizar_producto.php?id='.$fila['id'].'" method="post" >
-		<td><input class="lbl_text" type="text" name="nombre" value="'.$fila['nombre'].'" ></td>	
-		<td><input class="lbl_text" type="text" name="cod" value="'.$fila['codigo'].'"></td>	
-		<td><input class="lbl_text" type="text" name="precio" value="'.$fila['precio'].'"></td>
-		<td><img src="imagen/'.$fila['imagen'].'"  width=100px height=100px style="border: 2px solid #ffffff;box-shadow: 0px 10px 15px rgba(0,0,0,0.4);" ></td>
-		<td><input type="submit" value="Actualizar" class="btn btn-primary"></td>		
-	</form>
-		<td><a href="php/eliminar_producto.php?id='.$fila['id'].'"><button class="btn btn-danger">Borrar</buton></td>
-	</tr>
-	';
-}
-	
-?>
-	</table>
-</div>
+
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/646c794df3.js"></script>
 </body>
+
 </html>
